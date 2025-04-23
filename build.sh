9f87edfcbc24f1df2c2459bd172ab28c7346ac92
347921622@qq.com <niceqwer55555>
2025-04-23 18:57:18 +0800

    1

--- build.sh
+++ build.sh
@@ -4,4 +4,5 @@ JMETER_VERSION=${JMETER_VERSION:-"5.1.1"}
 IMAGE_TIMEZONE=${IMAGE_TIMEZONE:-"Asia/Shanghai"}
 
 # Example build line
+tar -xzvf ${JMETER_VERSION}.tar.gz
 docker build  --build-arg JMETER_VERSION=${JMETER_VERSION} --build-arg TZ=${IMAGE_TIMEZONE} -t "jmeter-slave:${JMETER_VERSION}" .
